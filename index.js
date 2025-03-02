c3a64cae2e175510ff38f7b2bcaafcb4f1fdcd9e
anthony anup <anup>
2025-03-02 19:12:47 +0530

    basic calculator

---
+++ index.js
@@ -0,0 +1,29 @@
+var clicked=""
+$("button").click(function(){
+
+    let value=$(this).attr('value')
+  
+
+    if (value =="="){
+    
+        $(".display").html(`<h1>${eval(clicked)}</h1>`)
+
+    }else if(value=="ac"){
+        clicked=""
+        $(".display").html(`<h1>${clicked}</h1>`)
+
+
+    }
+    else if(value=="back"){
+        clicked=clicked.slice(0,-1)
+        $(".display").html(`<h1>${clicked}</h1>`)
+    }else{
+        clicked+= value
+        $(".display").html(`<h1>${clicked}</h1>`)
+
+        
+    }
+    
+})
+
+
